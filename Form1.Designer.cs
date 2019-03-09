b14da42b2a6d7803e505053887e1e73da6601c09
Walkinged <43610419+Walkinged@users.noreply.github.com>
2019-03-09 17:01:32 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Timer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timer.Location = new System.Drawing.Point(147, 47);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(212, 55);
+            this.Timer.TabIndex = 0;
+            this.Timer.Text = "00:00:00";
+            this.Timer.Click += new System.EventHandler(this.Timer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 155);
+            this.Controls.Add(this.Timer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            //time
+
+            string time = "";
+
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+
+            else
+            {
+                time += hh;
+            }
+
+            time += ":"; 
+
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+
+            else
+            {
+                time += mm;
+            }
+
+            time += ":";
+
+            if (ss < 10)
+
+            {
+                time += "0" + ss;
+            }
+
+            else
+            {
+                time += ss;
+            }
+
+            time += ":";
+
+            //Update label
+
+            Timer.Text = time;
+
+        }
+
+        private void Timer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Timer Interval
+            t.Interval = 1000;      // In milliseconds
+
+            t.Tick += new EventHandler(this.t_Tick);
+
+            //Start
+
+            t.Start();
+        }
+    }
+}
